3159b3c9f79ad3386fe961d793f985e2bc13cdaa
Yurim Lee <u007x2@gmail.com>
2025-02-04 13:05:07 +0900

    client: 초기환경 세팅, 버튼 및 input field 필수 컴포넌트 추가

---
+++ 2_stream_video/video-streaming-client/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Video Streaming Client</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 2_stream_video/video-streaming-client/src/components/pages/StreamMainPage.tsx
@@ -0,0 +1,8 @@
+
+const StreamMainPage = () => {
+  return (
+    <div>StreamMainPage</div>
+  )
+}
+
+export default StreamMainPage
